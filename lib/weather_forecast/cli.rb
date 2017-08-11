5bc28b741b51381cd57ddac776a8288527a2b45a
sarahschaller <sarcsch@aol.com>
2017-08-11 17:08:28 -0400

    add methods to CLI

--- lib/weather_forecast/cli.rb
+++ lib/weather_forecast/cli.rb
@@ -2,11 +2,12 @@ class WeatherForecast::CLI
 
 	def call 
 		list_cities
+		choose_city
 	end
 
 	def list_cities 
-		puts "Choose a number to see the current weather forecast:"
-		#here doc's are like giant strings"
+		puts "Choose a city to see the current weather forecast:"
+		#'here doc's are like giant strings"
 		puts <<-DOC.gsub /^\s*/, ''
 			1.  New York, NY
 			2. Boston, MA
@@ -20,5 +21,23 @@ class WeatherForecast::CLI
 			10. Hartford, CT
 		DOC
 	end
+
+	def choose_city
+		input = gets.strip
+		until input.to_i > 0 && input.to_i < 11
+			puts "Please enter a number 1-10"
+			input = gets.strip
+		end
+		forecast_list
+    end
+
+    def forecast_list
+    	puts "Do you want to know the weather:"
+    	puts "1. Right now"
+    	puts "2. Hourly"
+    	puts "3. 5 day forecast"
+    end
+
+
 
 end
