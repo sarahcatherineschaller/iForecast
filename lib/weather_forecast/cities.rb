f0153735465f40ffc9b1dde79b82c20761e3b7f0
sarahschaller <sarcsch@aol.com>
2017-08-21 14:49:11 -0400

    create objects

--- lib/weather_forecast/cities.rb
+++ lib/weather_forecast/cities.rb
@@ -15,6 +15,22 @@ class WeatherForecast::Cities
 			9. Denver, CO
 			10. Hartford, CT
 		DOC
+
+		city_1 = self.new
+		city_1.name = "New York, NY"
+		city_1.temperature = 84
+		city_1.humidity = "89%"
+		city_1.forecast = "Mostly cloudy"
+		city_1.url = "https://weather.com/weather/today/l/USNY0996:1:US"
+
+		city_2 = self.new
+		city_2.name = "Boston, MA"
+		city_2.temperature = 82
+		city_2.humidity = "43%"
+		city_2.forecast = "Partly cloudy"
+		city_2.url = "https://weather.com/weather/today/l/USMA0046:1:US"
+
+		[city_1, city_2]
 	end
 
 
--- lib/weather_forecast/cli.rb
+++ lib/weather_forecast/cli.rb
@@ -8,7 +8,7 @@ class WeatherForecast::CLI
 		# puts "Choose a city to see the current weather forecast:"
 		# #'here doc's are like giant strings"
 		# puts <<-DOC.gsub /^\s*/, ''
-		# 	1.  New York, NY
+		# 	1. New York, NY
 		# 	2. Boston, MA
 		# 	3. San Francisco, CA
 		# 	4. Austin, TX 
@@ -32,35 +32,27 @@ class WeatherForecast::CLI
 		forecast_list
     end
 
-    def forecast_list
-    	puts "Do you want to know the weather:"
-    	puts "1. Right now"
-    	puts "2. Hourly"
-    	puts "3. 5 day forecast"
-    	forecast_choice
-    end
-
     def forecast_choice
     	input = gets.strip
     	until input.to_i > 0 && input.to_i < 4
 			puts "Please enter a number 1-3"
 			input = gets.strip
 		end
-		case input 
-		when "1"
-			puts "The temperature is currently 72° F"
-		when "2"
-			puts "6:00pm: Showers, 67° F"
-			puts "7:00pm: Few Showers, 67° F"
-			puts "8:00pm: Cloudy, 68° F"
-			puts "9:00pm: Cloudy, 69° F"
-		when "3"
-			puts "Tuesday:     Cloudy   —   High/Low  82/64   —   Precip  10%"
-			puts "Wednesday:   Sunny   —   High/Low  84/68   —   Precip  0%"
-			puts "Thursday:    Partly Cloudy   —   High/Low  82/68   —   Precip  0%"
-			puts "Friday:      PM Rain   —   High/Low  78/69   —   Precip  60%"
-			puts "Saturday:    Cloudy   —   High/Low  82/70   —   Precip  20%"
-		end
+		# case input 
+		# when "1"
+		# 	puts "The temperature is currently 72° F"
+		# when "2"
+		# 	puts "6:00pm: Showers, 67° F"
+		# 	puts "7:00pm: Few Showers, 67° F"
+		# 	puts "8:00pm: Cloudy, 68° F"
+		# 	puts "9:00pm: Cloudy, 69° F"
+		# when "3"
+		# 	puts "Tuesday:     Cloudy   —   High/Low  82/64   —   Precip  10%"
+		# 	puts "Wednesday:   Sunny   —   High/Low  84/68   —   Precip  0%"
+		# 	puts "Thursday:    Partly Cloudy   —   High/Low  82/68   —   Precip  0%"
+		# 	puts "Friday:      PM Rain   —   High/Low  78/69   —   Precip  60%"
+		# 	puts "Saturday:    Cloudy   —   High/Low  82/70   —   Precip  20%"
+		# end
 		puts start_over
 	end
 
