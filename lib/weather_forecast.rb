7cc28488acd920b008fa5f319ce977c5b015a69d
sarahschaller <sarcsch@aol.com>
2017-08-21 16:29:06 -0400

    require nokogiri

--- lib/weather_forecast.rb
+++ lib/weather_forecast.rb
@@ -1,3 +1,7 @@
+require "open-uri"
+require "nokogiri"
+require "pry"
+
 require_relative "weather_forecast/version"
 require_relative "weather_forecast/cities"
 require_relative "weather_forecast/cli"
