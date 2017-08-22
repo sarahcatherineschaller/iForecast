be2f054287a26d8a14d90a87b39b498690f7b015
sarahschaller <sarcsch@aol.com>
2017-08-22 13:38:28 -0400

    update CLI

--- lib/weather_forecast/cities.rb
+++ lib/weather_forecast/cities.rb
@@ -1,38 +1,19 @@
 class WeatherForecast::Cities
-	attr_accessor :name, :temperature, :humidity, :forecast, :url
+	attr_accessor :city_name, :temperature, :humidity, :visibility, :forecast, :url
 
-	def self.today
-		puts "Choose a city to see the current weather forecast:"
-		#'here doc's are like giant strings"
-		puts <<-DOC.gsub /^\s*/, ''
-			1. New York, NY
-			2. Boston, MA
-			3. San Francisco, CA
-			4. Austin, TX 
-			5. Seattle, WA
-			6. Orlando, FL
-			7. Phoenix, AZ
-			8. Washington, D.C.
-			9. Denver, CO
-			10. Hartford, CT
-		DOC
+    @@all = []
 
-		city_1 = self.new
-		city_1.name = "New York, NY"
-		city_1.temperature = 84
-		city_1.humidity = "89%"
-		city_1.forecast = "Mostly cloudy"
-		city_1.url = "https://weather.com/weather/today/l/USNY0996:1:US"
+    def self.all 
+    	@@all 
+    end
 
-		city_2 = self.new
-		city_2.name = "Boston, MA"
-		city_2.temperature = 82
-		city_2.humidity = "43%"
-		city_2.forecast = "Partly cloudy"
-		city_2.url = "https://weather.com/weather/today/l/USMA0046:1:US"
+    def save 
+    	@@all << self 
+    end
 
-		[city_1, city_2]
-	end
+    def self.clear_all
+    	@@all.clear 
+    end
 
 
 end
--- lib/weather_forecast/cli.rb
+++ lib/weather_forecast/cli.rb
@@ -5,56 +5,62 @@ class WeatherForecast::CLI
 	end
 
 	def list_cities 
-		# puts "Choose a city to see the current weather forecast:"
-		# #'here doc's are like giant strings"
-		# puts <<-DOC.gsub /^\s*/, ''
-		# 	1. New York, NY
-		# 	2. Boston, MA
-		# 	3. San Francisco, CA
-		# 	4. Austin, TX 
-		# 	5. Seattle, WA
-		# 	6. Orlando, FL
-		# 	7. Phoenix, AZ
-		# 	8. Washington, D.C.
-		# 	9. Denver, CO
-		# 	10. Hartford, CT
-		# DOC
-		@cities = WeatherForecast::Cities.today
-		choose_city
+
+		puts "Choose a city to see the current weather forecast:"
+		WeatherForecast::Cities.clear_all 
+		city_menu
 	end
 
-	def choose_city
+	def city_menu
+	    puts <<~DOC
+		 	1. New York, NY
+		 	2. Boston, MA
+		 	3. San Francisco, CA
+		 	4. Austin, TX 
+			5. Seattle, WA
+		 	6. Orlando, FL
+		 	7. Phoenix, AZ
+			8. Washington, D.C.
+			9. Denver, CO
+		 	10. Hartford, CT
+		DOC
 		input = gets.strip
 		until input.to_i > 0 && input.to_i < 11
 			puts "Please enter a number 1-10"
 			input = gets.strip
 		end
-		forecast_list
+		forecast_type
     end
 
-    def forecast_choice
-    	input = gets.strip
-    	until input.to_i > 0 && input.to_i < 4
-			puts "Please enter a number 1-3"
-			input = gets.strip
-		end
-		# case input 
-		# when "1"
-		# 	puts "The temperature is currently 72° F"
-		# when "2"
-		# 	puts "6:00pm: Showers, 67° F"
-		# 	puts "7:00pm: Few Showers, 67° F"
-		# 	puts "8:00pm: Cloudy, 68° F"
-		# 	puts "9:00pm: Cloudy, 69° F"
-		# when "3"
-		# 	puts "Tuesday:     Cloudy   —   High/Low  82/64   —   Precip  10%"
-		# 	puts "Wednesday:   Sunny   —   High/Low  84/68   —   Precip  0%"
-		# 	puts "Thursday:    Partly Cloudy   —   High/Low  82/68   —   Precip  0%"
-		# 	puts "Friday:      PM Rain   —   High/Low  78/69   —   Precip  60%"
-		# 	puts "Saturday:    Cloudy   —   High/Low  82/70   —   Precip  20%"
-		# end
-		puts start_over
+    def forecast_type
+    	puts "Choose a number to see the current forecast:"
+    	puts <<~DOC
+    	    1. Forecast
+    	    2. Temperature
+    	    3. Visibility
+    	    4. Humidity
+        DOC
+        forecast_choice
 	end
+
+	def forecast_choice
+		input = gets.strip
+        until input.to_i > 0 && input.to_i < 5
+        	puts "Please enter a number 1-4"
+        	input = gets.strip
+        end
+		if input == "1"
+			puts "Mostly Cloudy"
+		elsif input == "2"
+			puts "It is currently 67 degrees F in New York, NY"
+		elsif input == "3"
+			puts "7.0 miles"
+		elsif input == "4"
+			puts "43 percent humidity"
+		end
+		start_over
+	end
+
 
 	def start_over 
 		puts "Enter 'cities' to see the list of cities again, or 'exit'"
@@ -69,8 +75,6 @@ class WeatherForecast::CLI
 			puts "Goodbye!"
 		end
 	end
-
-
 
 
 
