ee4e09f05d60af25f509c9c7dc583cf5b7efec02
sarahschaller <sarcsch@aol.com>
2017-08-22 17:42:19 -0400

    create fake objects

--- lib/weather_forecast/cities.rb
+++ lib/weather_forecast/cities.rb
@@ -1,7 +1,31 @@
+
+require 'pry'
 class WeatherForecast::Cities
-	attr_accessor :city_name, :temperature, :humidity, :visibility, :forecast, :url
+	attr_accessor :name, :temperature, :humidity, :visibility, :forecast, :url
 
     @@all = []
+
+    def self.today
+    	city_1 = self.new
+    	city_1.name = "New York, NY"
+    	city_1.temperature = "67 degrees F"
+    	city_1.humidity = "43 percent"
+    	city_1.visibility = "7.0 miles"
+    	city_1.forecast = "Partly cloudy"
+    	city_1.url = "https://weather.com/weather/today/l/USNY0996:1:US"
+
+    	city_2 = self.new
+    	city_2.name = "Boston, MA"
+    	city_2.temperature = "64 degrees F"
+    	city_2.humidity = "10 percent"
+    	city_2.visibility = "10.0 miles"
+    	city_2.forecast = "Sunny"
+    	city_2.url = "https://weather.com/weather/today/l/USMA0046:1:US"
+
+    	[city_1, city_2]
+    end
+
+
 
     def self.all 
     	@@all 
--- lib/weather_forecast/scraper.rb
+++ lib/weather_forecast/scraper.rb
@@ -1,4 +1,16 @@
+require 'pry'
+
 class WeatherForecast::Scraper
+
+	def self.scrape_newyork
+		doc = Nokogiri::HTML(open("https://weather.com/weather/today/l/USNY0996:1:US"))
+		doc.css
+		binding.pry
+	end
+
+
+
+	#today_nowcard-phrase
 
 
 end
