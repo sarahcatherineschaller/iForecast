5e9807b1cfb8c06b00116896145ffa80cb5f5681
sarahschaller <sarcsch@aol.com>
2017-08-23 13:12:51 -0400

    scrape new york

--- lib/weather_forecast/cities.rb
+++ lib/weather_forecast/cities.rb
@@ -1,5 +1,6 @@
 
 require 'pry'
+
 class WeatherForecast::Cities
 	attr_accessor :name, :temperature, :humidity, :visibility, :forecast, :url
 
@@ -10,24 +11,44 @@ class WeatherForecast::Cities
     end
 
     def scrape_cities
-    	city_1 = self.new
-    	city_1.name = "New York, NY"
-    	city_1.temperature = "67 degrees F"
-    	city_1.humidity = "43 percent"
-    	city_1.visibility = "7.0 miles"
-    	city_1.forecast = "Partly cloudy"
-    	city_1.url = "https://weather.com/weather/today/l/USNY0996:1:US"
+    	cities = []
 
-    	city_2 = self.new
-    	city_2.name = "Boston, MA"
-    	city_2.temperature = "64 degrees F"
-    	city_2.humidity = "10 percent"
-    	city_2.visibility = "10.0 miles"
-    	city_2.forecast = "Sunny"
-    	city_2.url = "https://weather.com/weather/today/l/USMA0046:1:US"
+    	cities << self.scrape_newyork
 
-    	[city_1, city_2]
+    	cities
+    	# city_1 = self.new
+    	# city_1.name = "New York, NY"
+    	# city_1.temperature = "67 degrees F"
+    	# city_1.humidity = "43 percent"
+    	# city_1.visibility = "7.0 miles"
+    	# city_1.forecast = "Partly cloudy"
+    	# city_1.url = "https://weather.com/weather/today/l/USNY0996:1:US"
+
+    	# city_2 = self.new
+    	# city_2.name = "Boston, MA"
+    	# city_2.temperature = "64 degrees F"
+    	# city_2.humidity = "10 percent"
+    	# city_2.visibility = "10.0 miles"
+    	# city_2.forecast = "Sunny"
+    	# city_2.url = "https://weather.com/weather/today/l/USMA0046:1:US"
+
+    	# [city_1, city_2]
     end
+
+    def self.scrape_newyork
+    	doc = Nokogiri::HTML(open("https://weather.com/weather/today/l/USNY0996:1:US"))
+        #forecast = doc.css(".today_nowcard-phrase").text
+        #temperature = doc.css(".today_nowcard-temp").text
+        #feels like = doc.css(".today_nowcard-feels").text
+        #high = doc.css(".deg-hilo-nowcard")[0].text
+        #low = doc.css(".deg-hilo-nowcard")[1].text
+        #humidity = doc.css(".wx-detail-value")[1].text
+        #sunrise = doc.css("#dp0-details-sunrise").text
+        #sunset = doc.css("#dp0-details-sunset").text
+
+    	binding.pry
+    end
+
 
     def self.all 
     	@@all 
